010f92c6f6642671de75d46b5baeaad1c4029b93
heinrich-marx <weriast98@gmail.com>
2021-07-02 18:51:44 +0300

    feat: if num with minus - delete him

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function reverse (n) {
-  
+ 
+        n = n.toString().split('');
+        if (n[0] == '-') {
+            n.shift(n[0]);
+            n = n.reverse().join('');
+            return n
+        }
+        else {
+            n = n.reverse().join('');
+            return n
+        } 
+        
+    
+    
 }
